65ee2992af8f39a323910dcee5ffeb8cb2926c03
Anurag Tiwari <120814417+anurag-tiwari-up@users.noreply.github.com>
2024-09-24 23:12:14 +0530

    Revert "Merge pull request #1 from alok421/main"
    This reverts commit 207cbf99e79acf4238933d16e8e6295fc2eac1a1, reversing
    changes made to 5dc6095f9c9605e4401ed60f8c1af311d7beaa71.

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-window.onload = function() {
-    generateAndDisplayCaptcha();
-};
-
-let btn = document.getElementById('generate_btn');
-let fillBtn = document.getElementById('fill_btn');
+let btn = document.getElementById('generate_btn')
 let captcha;
-
 btn.addEventListener('click', function() {
-    generateAndDisplayCaptcha();
-});
-
-fillBtn.addEventListener('click', function() {
-    document.getElementById('text_input').value = captcha;
-});
-
-function generateAndDisplayCaptcha() {
-    captcha = generate_captcha(5);
+    captcha = generate_captcha(5); // Change the number to adjust string length
     document.getElementById('captcha').innerText = captcha;
-}
+});
 
 function generate_captcha(length) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -30,19 +16,16 @@ function generate_captcha(length) {
 }
 
 let input_text;
-let input = document.getElementById('input');
-input.addEventListener('submit', function(event) {
+
+let input = document.getElementById('input')
+input.addEventListener('submit',function(event){
     event.preventDefault();
     input_text = document.getElementById('text_input').value;
     let captcha_verification_result = document.getElementById('captcha_verification_result');
-    
-    captcha_verification_result.classList.remove("right", "error");
-    
-    if (captcha === input_text) {
-        captcha_verification_result.classList.add("right");
-        captcha_verification_result.innerText = "Correct! ✅";
-    } else {
-        captcha_verification_result.classList.add("error");
-        captcha_verification_result.innerText = "Try Again! ❌";
+    if(captcha===input_text){
+        captcha_verification_result.innerText = "sahi hai bhai"
     }
-});
+    else{
+        captcha_verification_result.innerText = "bhak sasur"
+    }
+})
