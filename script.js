a8d8022f02817755b90d32f0fee3aa6e952ec969
Anurag Tiwari <120814417+anurag-tiwari-up@users.noreply.github.com>
2024-09-24 19:30:26 +0530

    added some logic

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let btn = document.getElementById('generate_btn')
+let captcha;
 btn.addEventListener('click', function() {
-    const captcha = generate_captcha(5); // Change the number to adjust string length
+    captcha = generate_captcha(5); // Change the number to adjust string length
     document.getElementById('captcha').innerText = captcha;
 });
 
@@ -13,3 +14,19 @@ function generate_captcha(length) {
     }
     return result;
 }
+
+let input_text;
+
+let input = document.getElementById('input')
+input.addEventListener('submit',function(event){
+    event.preventDefault();
+    input_text = document.getElementById('text_input').value;
+    let captcha_verification_result = document.getElementById('captcha_verification_result');
+    if(captcha===input_text){
+        captcha_verification_result.innerText = "sahi hai bhai"
+    }
+    else{
+        captcha_verification_result.innerText = "bhak sasur"
+        
+    }
+})
