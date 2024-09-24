2e4b077c5dd29dcbe7d4066527d4438867af2267
alok421 <166999174+alok421@users.noreply.github.com>
2024-09-24 20:19:42 +0530

    Add functionality to autofill captcha

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-let btn = document.getElementById('generate_btn')
+window.onload = function() {
+    generateAndDisplayCaptcha();
+};
+
+let btn = document.getElementById('generate_btn');
+let fillBtn = document.getElementById('fill_btn');
 let captcha;
+
 btn.addEventListener('click', function() {
-    captcha = generate_captcha(5); // Change the number to adjust string length
-    document.getElementById('captcha').innerText = captcha;
+    generateAndDisplayCaptcha();
 });
+
+fillBtn.addEventListener('click', function() {
+    document.getElementById('text_input').value = captcha;
+});
+
+function generateAndDisplayCaptcha() {
+    captcha = generate_captcha(5);
+    document.getElementById('captcha').innerText = captcha;
+}
 
 function generate_captcha(length) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -16,16 +30,19 @@ function generate_captcha(length) {
 }
 
 let input_text;
-
-let input = document.getElementById('input')
-input.addEventListener('submit',function(event){
+let input = document.getElementById('input');
+input.addEventListener('submit', function(event) {
     event.preventDefault();
     input_text = document.getElementById('text_input').value;
     let captcha_verification_result = document.getElementById('captcha_verification_result');
-    if(captcha===input_text){
-        captcha_verification_result.innerText = "sahi hai bhai"
+    
+    captcha_verification_result.classList.remove("right", "error");
+    
+    if (captcha === input_text) {
+        captcha_verification_result.classList.add("right");
+        captcha_verification_result.innerText = "Correct! ✅";
+    } else {
+        captcha_verification_result.classList.add("error");
+        captcha_verification_result.innerText = "Try Again! ❌";
     }
-    else{
-        captcha_verification_result.innerText = "bhak sasur"
-    }
-})
+});
